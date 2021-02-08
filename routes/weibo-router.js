79938e38ab6c1f05300f5be82016689ab344aec2
jay <jay.jia@livenaked.com>
2021-02-08 15:49:32 +0800

    upgrade-api weibo/login

--- routes/weibo-router.js
+++ routes/weibo-router.js
@@ -1,4 +1,5 @@
 const weibo = require('weibo');
+const axios = require('axios');
 const router = require('koa-router')()
 
 router.prefix('/weibo')
@@ -8,17 +9,23 @@ router.get('/login', function (ctx, next) {
   var appkey = '1120585538';
   var secret = '36ba3598818d0fef65809e8d03305be1';
   var oauth_callback_url = 'https://redsapi-9gtejk3n12548b8d-1258543641.ap-shanghai.app.tcloudbase.com/redskoa/weibo/cb';
-  weibo.init('weibo', appkey, secret, oauth_callback_url);
-
-  var user = { blogtype: 'weibo' };
-  var cursor = { count: 20 };
-  weibo.public_timeline(user, cursor, function (err, statuses) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(statuses);
+  
+  axios.get('https://api.weibo.com/oauth2/authorize', {
+    params: {
+      client_id: appkey,
+      redirect_uri: 'https://redsapi-9gtejk3n12548b8d-1258543641.ap-shanghai.app.tcloudbase.com/redskoa/weibo/cb',
+      response_type: 'code'
     }
-  });
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });  
   ctx.body = 'this is a users/bar response'
 })
 
@@ -27,6 +34,7 @@ router.get('/cb', function (ctx, next) {
   // 从上下文的request对象中获取
   let request = ctx.request
   let req_query = request.query
+  console.log(req_query);
   let req_querystring = request.querystring
 
   // 从上下文中直接获取
